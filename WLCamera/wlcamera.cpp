3f20ef865dac37dbfb9b9b479f7dbb1e6e34162b
Chao Fan <cfan@perimetermed.com>
2017-02-24 18:27:02 -0500

    office

--- WLCamera/wlcamera.cpp
+++ WLCamera/wlcamera.cpp
@@ -26,60 +26,92 @@ WLCamera::WLCamera()
     PylonInitialize();//Camera initialization
     CDeviceInfo info;
     info.SetDeviceClass( Camera_t::DeviceClass());
-    Camera_t camera( CTlFactory::GetInstance().CreateFirstDevice(info));
-    camera.Open();
-    //camera.Gain.SetValue(50000);
-    if (IsWritable(camera.GainAuto))
+    try
     {
-        camera.GainAuto.FromString("Off");
-    }
-    camera.Gain.SetValue((camera.Gain.GetMin() + camera.Gain.GetMax())/2 );
-    camera.ExposureTime.SetValue(40000.0);
-    cout << "using device" << camera.GetDeviceInfo().GetModelName() << endl;
-    camera.StartGrabbing(c_countOfImagesToGrab, GrabStrategy_LatestImageOnly);
-    CGrabResultPtr ptrGrabResult;
-    CPylonImageWindow imageWindow;
-    while (camera.IsGrabbing())
+        if (~info.IsModelNameAvailable())
+            throw "The USB Camera is not connected";
+        else
         {
-            //wait for an iamge and then retrive it, a timeout of 5000ms is used
-            camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
+            Camera_t camera( CTlFactory::GetInstance().CreateFirstDevice(info));
+            camera.Open();
+            // Camera Configuration
+            //Pixel format
+            //camera.PixelFormat.SetValue(PixelFormat_YCbCr422_8);
+            // Reverse X/Reverse Y
+            //camera.ReverseX.SetValue(false); // Disable x rotation
+            //camera.ReverseY.SetValue(false); // Disable y rotation
+            // Binning, it is needed to improve the display frequency
+            //camera.BinningVertical.SetValue(1); // Disable vertical binning
+            //camera.BinningHorizontal.SetValue(1); //Disable horizontal binning
+            // Image ROI, default no ROI
+            /*int64_t CameraWidthMax = camera.WidthMax.GetValue();
+            int64_t CameraHeightmax = camera.HeightMax.GetValue();
+            cout << "Camera dimension" << CameraWidthMax << "x" << CameraHeightmax << endl;
+            camera.Width.SetValue(CameraWidthMax);
+            camera.Height.SetValue(CameraHeightmax);
+            camera.OffsetY.SetValue(0);
+            camera.OffsetX.SetValue(0);*/
+            // Black level
+            // Gain
+            /* if (IsWritable(camera.GainAuto))
+             {
+                 camera.GainAuto.FromString("On"); //set to AutoGain
+             }*/
+            // Light source preset
+            // Balance white
+            // Gamma
+            // Hue/Saturation
+            // Brightness / Contrast
+            //PGI feature set
+            // Defect pixel correction
+            // Backlight compenstaion
 
-            if (ptrGrabResult->GrabSucceeded())
+            //camera.Gain.SetValue(50000);
+
+
+            //camera.ExposureTime.SetValue(40000.0);
+            cout << "using device" << camera.GetDeviceInfo().GetModelName() << endl;
+            camera.StartGrabbing(c_countOfImagesToGrab, GrabStrategy_LatestImageOnly);
+            CGrabResultPtr ptrGrabResult;
+            CPylonImageWindow imageWindow;
+            while (camera.IsGrabbing())
             {
-
-                cout << "Grab OK!" <<endl;
-                CPylonImage image;
-                image.AttachGrabResultBuffer(ptrGrabResult); // attach grab result to a pylon image
-                //compute the iamge size
-                uint32_t imageWidth = image.GetWidth();
-                uint32_t imageHeight = image.GetHeight();
-                // creat the window and display the image
-                bool windowCreated = false;
-                if (!imageWindow.IsValid())
+                //wait for an iamge and then retrive it, a timeout of 5000ms is used
+                camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
+                if (ptrGrabResult->GrabSucceeded())
                 {
-                    imageWindow.Create(1, 400, 400, imageWidth, imageHeight);
-                    imageWindow.SetImage(image);
-                    //imageWindow.Show();
-                    cout << "image window is shown" << endl;
+                    cout << "Grab OK!" << endl;
+                    CPylonImage image;
+                    image.AttachGrabResultBuffer(ptrGrabResult); // attach grab result to a pylon image
+                    //compute the iamge size
+                    uint32_t imageWidth = image.GetWidth();
+                    uint32_t imageHeight = image.GetHeight();
+                    // creat the window and display the image
+                    bool windowCreated = false;
+                    if (!imageWindow.IsValid())
+                    {
+                        imageWindow.Create(1, 400, 400, imageWidth, imageHeight);
+                        imageWindow.SetImage(image);
+                        //imageWindow.Show();
+                        cout << "image window is shown" << endl;
+                    }
+                    windowCreated = true;
+                    imageWindow.Show();
                 }
-                windowCreated = true;
-                imageWindow.Show();
+            }
 
+            while (1)
+            {
+                // keep display the image
 
             }
         }
-
-        while(1)
-        {
-           // keep display the image
-
-        }
-        // Releases all pylon resources.
-        PylonTerminate();
-
-
-
-
+    }
+    catch (char* msg)
+    {
+        cerr << "Camera is not connected" << endl;
+        cerr << *msg << endl;
+    }
 
 }
 
